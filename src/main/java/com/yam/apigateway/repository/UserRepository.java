d4439fe10441d1a38505b73a76a6fb97a02a04c3
dy <51532891+dzero-learn@users.noreply.github.com>
2026-07-09 10:39:28 +0900

    chore: userRepository 메소드명 변경

--- src/main/java/com/yam/apigateway/repository/UserRepository.java
+++ src/main/java/com/yam/apigateway/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findUsersByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
